d587faab25184e838b83eb19abc4062cd6cf4451
Separ-vivi <838200633@qq.com>
2026-08-27 21:41:24 +0800

    V6.20: update frontend/src/router/index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -54,7 +54,7 @@ const router = createRouter({
 })
 
 router.afterEach((to) => {
-  document.title = `${to.meta?.title || '辅导员工作平台'} · V6.18`
+  document.title = `${to.meta?.title || '辅导员工作平台'} · V6.20`
 })
 
 export default router
